baf02226d9278e1e62e09f66d102dc048e1abef6
Bruno Buck Brigagao <bruno.brigagao@uniriotec.br>
2015-10-27 21:03:33 -0200

    Adicionado contador de vida.

--- src/jogo/Jogo.java
+++ src/jogo/Jogo.java
@@ -12,14 +12,14 @@ import java.util.Scanner;
 
 /**
  *
- * @author ALAN
+ * @author ALAN e BRUNO
  */
 public class Jogo { 
     
     private int dificuldade;
     
     public String setOperacao(){
-        String oper;
+        String operacao;
         Scanner scan=new Scanner(System.in);      
         System.out.println("------------------------");
         System.out.println("Escolha uma operacao: ");
@@ -30,13 +30,13 @@ public class Jogo {
         System.out.println("/ : Divisao");
         System.out.println("------------------------");
         System.out.print("Operacao: ");
-        oper=scan.next(); 
-        return oper;
+        operacao=scan.next();
+        return operacao;
     }
     
     public void setDificuldade(){
         
-        Scanner scan=new Scanner(System.in);      
+        Scanner leia = new Scanner(System.in);
         System.out.println("------------------------");
         System.out.println("Escolha uma dificuldade: ");
         System.out.println("------------------------");
@@ -46,7 +46,7 @@ public class Jogo {
         System.out.println("4 : Nivel 4");
         System.out.println("------------------------");
         System.out.print("Dificuldade: ");
-        dificuldade=scan.nextInt();               
+        dificuldade=leia.nextInt();
     }    
     
     public int getDificuldade(){
@@ -54,17 +54,17 @@ public class Jogo {
     }
     
     public int setResposta(){
-        int resp;
+        int resposta;
         System.out.println("");
         Scanner entrada=new Scanner(System.in);
-        resp=entrada.nextInt();  
-        return resp;
+        resposta=entrada.nextInt();
+        return resposta;
     }
     
     public static void embaralhar(int[] opcao){
-        Random randon = new Random();
+        Random gera = new Random();
         for(int i = 0; i < opcao.length; i++){
-          int pos = randon.nextInt(opcao.length);
+          int pos = gera.nextInt(opcao.length);
           int temp = opcao[i];
           opcao[i] = opcao[pos];
           opcao[pos] = temp;
@@ -83,10 +83,11 @@ public class Jogo {
         int totalAcertos=0;
         int totalErros=0;
         int numPergunta=1;
+        int vida=3;
         
         // TODO code application logic here  
         String escolha;
-        int resp;
+        int resposta;
         System.out.println("-----------------------------------");
         System.out.println("Bem-vindo ao Matematica Divertida! ");
         System.out.println("-----------------------------------");
@@ -102,27 +103,27 @@ public class Jogo {
             switch (escolha){
                 /*-----------------------------------------*/
                 case "+":
-                    for (int i=0;i<5;i++){
+                    for (int i=0;i<5 && vida>0;i++){
                         System.out.println("Pergunta " + numPergunta);
-                        resp = oper.Adicao();
+                        resposta = oper.Adicao();
                         System.out.println("");                    
                         // inicializa os elementos do array
-                        opcao[0] = (resp + ((int)(Math.random()*20+2)));
-                        opcao[1] = (resp + ((int)(Math.random()*30+1)));
-                        opcao[2] = (resp + ((int)(Math.random()*10)));
-                        opcao[3] = resp;                
+                        opcao[0] = (resposta + ((int)(Math.random()*20+2)));
+                        opcao[1] = (resposta + ((int)(Math.random()*30+1)));
+                        opcao[2] = (resposta + ((int)(Math.random()*10)));
+                        opcao[3] = resposta;
                         // embaralha o array
                          embaralhar(opcao);
-                         if ( resp == user.setResposta()){ 
+                         if ( resposta == user.setResposta()){
                             System.out.println("Correto");  
                             totalAcertos++;
                         }else{
-                                System.out.println("Errado");
-                                System.out.println("Resposta:" + resp);
-                                totalErros++;
-                                
-                                }
-                         numPergunta++;
+                            System.out.println("Errado");
+                            System.out.println("Resposta:" + resposta);
+                            totalErros++;
+                            vida--;
+                        }
+                        numPergunta++;
                     }//for
                     historico.setPercAcertos(totalAcertos);
                     historico.setQtdErros(totalErros);
@@ -132,32 +133,34 @@ public class Jogo {
                     System.out.println(totalAcertos);
                     System.out.println("-----------Pontuacao--------------------------");
                     System.out.println("Nivel Atual: " + historico.getNivelAtual());
-                    System.out.println("% Acertos: " + historico.getPerctAcertos());  
+                    System.out.println("Acertos: " + historico.getPerctAcertos());
                     System.out.println("Total de Pontos: " + historico.getTotalPontos());                    
                     System.out.println("Qtd Erros: " + historico.getQtdErros()); 
                     System.out.println("Proximo nivel: " + historico.getNumeroPontosProxNivel() + " pontos");
+                    System.out.println("Vidas: " + vida);
                     System.out.println("----------------------------------------------");
                     break;
                     /*-----------------------------------------*/
                     case "-":
-                        for (int i=0;i<5;i++){
+                        for (int i=0;i<5 && vida>0;i++){
                             System.out.println("Pergunta " + numPergunta);    
-                            resp = oper.Subtracao();
+                            resposta = oper.Subtracao();
                             System.out.println("");                    
                             // inicializa os elementos do array
-                            opcao[0] = (resp + ((int)(Math.random()*20+2)));
-                            opcao[1] = (resp + ((int)(Math.random()*30+1)));
-                            opcao[2] = (resp + ((int)(Math.random()*10)));
-                            opcao[3] = resp;                
+                            opcao[0] = (resposta + ((int)(Math.random()*20+2)));
+                            opcao[1] = (resposta + ((int)(Math.random()*30+1)));
+                            opcao[2] = (resposta + ((int)(Math.random()*10)));
+                            opcao[3] = resposta;
                             // embaralha o array
                              embaralhar(opcao);
-                            if ( resp == user.setResposta()){ 
+                            if ( resposta == user.setResposta()){
                                 System.out.println("Correto");  
                                  totalAcertos++;
                             }else{
                                     System.out.println("Errado");
-                                    System.out.println("Resposta:" + resp);
+                                    System.out.println("Resposta:" + resposta);
                                     totalErros++;
+                                    vida--;
                                     } 
                                 numPergunta++;
                      }//for
@@ -169,31 +172,33 @@ public class Jogo {
                     System.out.println(totalAcertos);
                     System.out.println("-----------Pontuacao--------------------------");
                     System.out.println("Nivel Atual: " + historico.getNivelAtual());
-                    System.out.println("% Acertos: " + historico.getPerctAcertos());  
+                    System.out.println("Acertos: " + historico.getPerctAcertos());
                     System.out.println("Total de Pontos: " + historico.getTotalPontos());                    
                     System.out.println("Qtd Erros: " + historico.getQtdErros()); 
                     System.out.println("Proximo nivel: " + historico.getNumeroPontosProxNivel() + " pontos");
+                    System.out.println("Vidas: " + vida);
                     System.out.println("----------------------------------------------");
                     break;
                     /*-----------------------------------------*/
                     case "*":
-                        for (int i=0;i<5;i++){
-                            resp = oper.Multiplicacao();
+                        for (int i=0;i<5 && vida>0;i++){
+                            resposta = oper.Multiplicacao();
                             System.out.println("");                    
                             // inicializa os elementos do array
-                            opcao[0] = (resp + ((int)(Math.random()*20+2)));
-                            opcao[1] = (resp + ((int)(Math.random()*30+1)));
-                            opcao[2] = (resp + ((int)(Math.random()*10)));
-                            opcao[3] = resp;                
+                            opcao[0] = (resposta + ((int)(Math.random()*20+2)));
+                            opcao[1] = (resposta + ((int)(Math.random()*30+1)));
+                            opcao[2] = (resposta + ((int)(Math.random()*10)));
+                            opcao[3] = resposta;
                             // embaralha o array
                              embaralhar(opcao);
-                            if ( resp == user.setResposta()){ 
+                            if ( resposta == user.setResposta()){
                                 System.out.println("Correto"); 
                                 totalAcertos++;
                             }else{
                                     System.out.println("Errado");
-                                    System.out.println("Resposta:" + resp);
+                                    System.out.println("Resposta:" + resposta);
                                     totalErros++;
+                                    vida--;
                                     } 
                             numPergunta++;
                         }//for
@@ -205,31 +210,33 @@ public class Jogo {
                     System.out.println(totalAcertos);
                     System.out.println("-----------Pontuacao--------------------------");
                     System.out.println("Nivel Atual: " + historico.getNivelAtual());
-                    System.out.println("% Acertos: " + historico.getPerctAcertos());  
+                    System.out.println("Acertos: " + historico.getPerctAcertos());
                     System.out.println("Total de Pontos: " + historico.getTotalPontos());                    
                     System.out.println("Qtd Erros: " + historico.getQtdErros()); 
                     System.out.println("Proximo nivel: " + historico.getNumeroPontosProxNivel() + " pontos");
+                    System.out.println("Vidas: " + vida);
                     System.out.println("----------------------------------------------");
                     break;
                     /*-----------------------------------------*/  
                     case "/":
-                        for (int i=0;i<5;i++){
-                            resp = oper.Divisao();
+                        for (int i=0;i<5 && vida>0;i++){
+                            resposta = oper.Divisao();
                             System.out.println("");                    
                             // inicializa os elementos do array
-                            opcao[0] = (resp + ((int)(Math.random()*20+2)));
-                            opcao[1] = (resp + ((int)(Math.random()*30+1)));
-                            opcao[2] = (resp + ((int)(Math.random()*10)));
-                            opcao[3] = resp;                
+                            opcao[0] = (resposta + ((int)(Math.random()*20+2)));
+                            opcao[1] = (resposta + ((int)(Math.random()*30+1)));
+                            opcao[2] = (resposta + ((int)(Math.random()*10)));
+                            opcao[3] = resposta;
                             // embaralha o array
                              embaralhar(opcao);
-                            if ( resp == user.setResposta()){ 
+                            if ( resposta == user.setResposta()){
                                 System.out.println("Correto");
                                 totalAcertos++;
                             }else{
                                     System.out.println("Errado");
-                                    System.out.println("Resposta:" + resp);
+                                    System.out.println("Resposta:" + resposta);
                                     totalErros++;
+                                    vida--;
                                     }
                          numPergunta++;
                         }//for
@@ -241,10 +248,11 @@ public class Jogo {
                     System.out.println(totalAcertos);
                     System.out.println("-----------Pontuacao--------------------------");
                     System.out.println("Nivel Atual: " + historico.getNivelAtual());
-                    System.out.println("% Acertos: " + historico.getPerctAcertos());  
+                    System.out.println("Acertos: " + historico.getPerctAcertos());
                     System.out.println("Total de Pontos: " + historico.getTotalPontos());                    
                     System.out.println("Qtd Erros: " + historico.getQtdErros()); 
                     System.out.println("Proximo nivel: " + historico.getNumeroPontosProxNivel() + " pontos");
+                    System.out.println("Vidas: " + vida);
                     System.out.println("----------------------------------------------");
                     break;
                     /*-----------------------------------------*/    
